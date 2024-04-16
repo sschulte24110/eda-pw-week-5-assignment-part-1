4d2af1ba311a7ac064be00f8c8b12f9a09fec920
Scott Schulte <scott.r.schulte@gmail.com>
2024-04-15 22:07:03 -0500

    Complete question 4. I have a lot of shoes, probably more than what I admitted on here.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -51,6 +51,9 @@ console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
   - Console.log your updated shoe count. 
 */
 
+console.log(me.shoeCount);
+me.shoeCount = me.shoeCount + 1;
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
